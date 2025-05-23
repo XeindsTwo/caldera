3cd16235a03622f902b22ee41d4d08c7f7a0a87a
Your Name <you@example.com>
2025-05-23 14:22:10 +0300

    Добавлен новый функционал, сделана верстка которая подстраивается под большую ширину и высоту экранов

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,57 @@
-import {setupMobileMenu} from "./mobileMenu.js";
+const blockOne = document.getElementById("blockOne");
+const blockTwo = document.getElementById("blockTwo");
+const btn = document.getElementById("checkEligibilityBtn");
+const content = document.querySelector(".home__content");
+const img = document.querySelector(".home__img");
 
-setupMobileMenu();
+btn.addEventListener("click", () => {
+  blockOne.classList.remove("active");
 
-document.addEventListener("DOMContentLoaded", function () {
-  const video = document.querySelector(".home__video");
-  video.removeAttribute("controls");
+  setTimeout(() => {
+    blockTwo.classList.add("active");
+    content.classList.add("expanded");
+    img.classList.add("shrunk");
+  }, 550);
 });
 
-function scrollToSection(event) {
-  event.preventDefault();
-  const targetId = event.target.getAttribute('href').slice(1);
-  const targetElement = document.getElementById(targetId);
+const input = document.getElementById("address");
+const arrowButton = document.getElementById("checkEthWalletBtn");
+const iconFilter = document.querySelector(".home__icon-filter");
+const iconCircle = document.querySelector(".home__icon-circle");
+const checkAllocationBtn = document.getElementById("checkAllocation");
 
-  let targetOffset;
+function checkInputValidation() {
+  const value = input.value.trim();
+  const isValid = value.startsWith("0x") && value.length === 42;
 
-  targetOffset = targetElement.offsetTop - 30;
-  window.scrollTo({top: targetOffset, behavior: 'smooth'});
+  if (isValid) {
+    arrowButton.classList.add("active");
+    arrowButton.disabled = false;
+  } else {
+    arrowButton.classList.remove("active");
+    arrowButton.disabled = true;
+    checkAllocationBtn.classList.remove("active");
+  }
 }
 
-const menuLinks = document.querySelectorAll('.desktop');
+input.addEventListener("input", checkInputValidation);
 
-menuLinks.forEach((menuLink) => {
-  menuLink.addEventListener('click', scrollToSection);
+arrowButton.addEventListener("click", function () {
+  const value = input.value.trim();
+  const isValid = value.startsWith("0x") && value.length === 42;
+  if (!isValid) return;
+
+  iconFilter.style.display = "none";
+  iconCircle.style.display = "inline";
+  arrowButton.disabled = true;
+
+  const delay = Math.floor(Math.random() * 2000) + 1000;
+
+  setTimeout(() => {
+    iconFilter.style.display = "inline";
+    iconCircle.style.display = "none";
+    arrowButton.disabled = false;
+    checkAllocationBtn.disabled = false;
+    console.log("Eligible!");
+  }, delay);
 });
